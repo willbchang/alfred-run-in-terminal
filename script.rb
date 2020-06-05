7a7af4c2701e4503834be116acb5fd87e8e3806b
Will B Chang <will.b.chang@gmail.com>
2020-06-05 21:37:14 +0800

    refactor: move variable outside function

--- script.rb
+++ script.rb
@@ -5,17 +5,18 @@ def cd_folder(terminal, path)
 end
 
 def run_command(terminal, command)
-  command = /^\$\s.*/.match?(command) ? command[2..-1] : command
   system %Q(osascript -e 'tell app "#{terminal.shellescape}" to do script "#{command}" activate')
 end
 
+terminal = ARGV[1].shellescape
 query = ARGV[0]
-terminal = ARGV[1]
 # Remove single quote around file path when selecting from Alfred File Browser
 filepath = /^'.*'$/.match?(query) ? query[1..-2] : query
+# Remove `$ ` in the beginning of the command, usually in stackoverflow.com or github.com
+command = /^\$\s.*/.match?(query) ? query[2..-1] : query
+
 if File.directory?(filepath)
   cd_folder(terminal, filepath)
 else
-  run_command(terminal, query)
-  
+  run_command(terminal, command)
 end
