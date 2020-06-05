efdf32323ec48c336726be8c3f79907b425058f1
Will B Chang <will.b.chang@gmail.com>
2020-06-05 21:32:39 +0800

    feat: open select path in terminal

--- script.rb
+++ script.rb
@@ -1,5 +1,8 @@
 require 'shellwords'
 
+def cd_folder(terminal, path)
+  system %Q(osascript -e 'tell app "#{terminal.shellescape}" to do script "cd #{path.shellescape}" activate')
+end
 
 def run_command(terminal, command)
   command = /^\$\s.*/.match?(command) ? command[2..-1] : command
@@ -8,5 +11,11 @@ end
 
 query = ARGV[0]
 terminal = ARGV[1]
-run_command(terminal, query)
-
+# Remove single quote around file path when selecting from Alfred File Browser
+filepath = /^'.*'$/.match?(query) ? query[1..-2] : query
+if File.directory?(filepath)
+  cd_folder(terminal, filepath)
+else
+  run_command(terminal, query)
+  
+end
