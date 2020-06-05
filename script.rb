f2fc537279b0943081031fcc7a655f84843da15b
Will B Chang <will.b.chang@gmail.com>
2020-06-05 15:03:28 +0800

    refactor: extract run_command()

--- script.rb
+++ script.rb
@@ -1,7 +1,12 @@
 require 'shellwords'
 
-command = ARGV[0]
+
+def run_command(terminal, command)
+  command = /^\$\s.*/.match?(command) ? command[2..-1] : command
+  system %Q(osascript -e 'tell app "#{terminal.shellescape}" to do script "#{command}" activate')
+end
+
+query = ARGV[0]
 terminal = ARGV[1]
-command = /^\$\s.*/.match?(command) ? command[2..-1] : command
-system %Q(osascript -e 'tell app "#{terminal.shellescape}" to do script "#{command}" activate')
+run_command(terminal, query)
 
