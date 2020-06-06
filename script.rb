93180510abf05dcf7a6724271a7726a3d0e8476a
Will B Chang <will.b.chang@gmail.com>
2020-06-06 08:23:33 +0800

    refactor: replace functions with variables

--- script.rb
+++ script.rb
@@ -1,22 +1,17 @@
 require 'shellwords'
 
-def cd_folder(terminal, path)
-  system %Q(osascript -e 'tell app "#{terminal.shellescape}" to do script "cd #{path.shellescape}" activate')
-end
 
-def run_command(terminal, command)
-  system %Q(osascript -e 'tell app "#{terminal.shellescape}" to do script "#{command}" activate')
-end
-
-terminal = ARGV[1].shellescape
 query = ARGV[0]
+terminal = ARGV[1].shellescape
 # Remove single quote around file path when selecting from Alfred File Browser
 filepath = /^'.*'$/.match?(query) ? query[1..-2] : query
 # Remove `$ ` in the beginning of the command, usually in stackoverflow.com or github.com
 command = /^\$\s.*/.match?(query) ? query[2..-1] : query
+# https://stackoverflow.com/a/14949613/9984029
+script = %Q(osascript -e 'tell app "#{terminal}" to do script "COMMAND" activate')
 
 if File.directory?(filepath)
-  cd_folder(terminal, filepath)
+  system script.sub('COMMAND', "cd #{filepath}")
 else
-  run_command(terminal, command)
+  system script.sub('COMMAND', command)
 end
