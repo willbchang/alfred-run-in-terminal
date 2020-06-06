2e4c95b363ba6ab273c85f8340829d1c74d71e1a
Will B Chang <will.b.chang@gmail.com>
2020-06-06 10:56:52 +0800

    refactor: extract script

--- script.rb
+++ script.rb
@@ -3,10 +3,10 @@ require 'shellwords'
 
 query = ARGV[0]
 terminal = ARGV[1].shellescape
+
 # Remove single quote around file path when selecting from Alfred File Browser
 filepath = /^'.*'$/.match?(query) ? query[1..-2] : query
-# Remove `$ ` in the beginning of the command, usually in stackoverflow.com or github.com
-command = /^\$\s.*/.match?(query) ? query[2..-1] : query
+
 # Executable programming languages' extension hash
 runtime_environments = {
   rb: 'ruby',
@@ -19,16 +19,16 @@ runtime_environments = {
   rs: 'rust'
 }
 
-# https://stackoverflow.com/a/14949613/9984029
-script = %Q(osascript -e 'tell app "#{terminal}" to do script "COMMAND" activate')
 
-p filepath
-p File.file?(filepath)
 if File.directory?(filepath)
-  system script.sub('COMMAND', "cd #{filepath}")
+  command = "cd #{filepath}"
 elsif File.file?(filepath)
   file_extension = File.extname(filepath)[1..-1]
-  system script.sub('COMMAND', "#{runtime_environments[file_extension.to_sym]} #{filepath}")
+  command = "#{runtime_environments[file_extension.to_sym]} #{filepath}"
 else
-  system script.sub('COMMAND', command)
+  # Remove `$ ` in the beginning of the command, usually in stackoverflow.com or github.com
+  command = /^\$\s.*/.match?(query) ? query[2..-1] : query
 end
+
+# https://stackoverflow.com/a/14949613/9984029
+`osascript -e 'tell app "#{terminal}" to do script "#{command}" activate'`
