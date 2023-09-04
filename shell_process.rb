97eb7f5385208605962264fa3026c6619c33036d
Will B Chang <will.b.chang@gmail.com>
2023-09-04 21:59:56 +0800

    refactor: extract function

--- shell_process.rb
+++ shell_process.rb
@@ -1,5 +1,12 @@
-tty = ARGV[0]
-old_tty_number = tty[/\d+$/]
-incremented_tty_number = (old_tty_number.to_i + 1).to_s.rjust(old_tty_number.length, '0')
-tty = tty.sub(old_tty_number, incremented_tty_number)
-print `ps -t #{tty} -o command= | tail -n 1`
+def increase_tty_number(tty)
+  old_tty_number = tty[/\d+$/]
+  incremented_tty_number = (old_tty_number.to_i + 1).to_s.rjust(old_tty_number.length, '0')
+  tty.sub(old_tty_number, incremented_tty_number)
+end
+
+def get_last_process(tty)
+  tty = increase_tty_number(tty)
+  print `ps -t #{tty} -o command= | tail -n 1`
+end
+
+get_last_process(ARGV[0])
